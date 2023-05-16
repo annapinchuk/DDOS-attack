5de65aefc06f6e901063b0c6a83b264e36d606d1
Zohar Simhon <Zoharsimhon13@gmail.com>
2023-05-16 23:53:27 +0300

    added graphs to the results txt

--- vols/attacker/graph.py
+++ vols/attacker/graph.py
@@ -1,25 +1,30 @@
 import matplotlib.pyplot as plt
 
+
 # Read data from the text file
 with open('syns_results_py.txt', 'r') as file:
     lines = file.readlines()
 
 # Extract the x and y values from the lines
-x_values = []
-y_values = []
-for line in lines[1:]:
-    data = line.strip().split('\t')
-    x_values.append(float(data[1]))
-    y_values.append(int(data[0]))
-    print(int(data[0]))
+x_values = []  # time
+y_values = []  # number of packets
+timer = 0
+counter = 0
+for line in lines:
+    counter += 1
+    data = line.split("\t")
+    time = float(data[1])
+    x_values .append(time)
+    y_values .append(data[0])
 
-# Plot the data
-plt.xlim(14,21792320)
-plt.ylim(0,1000000)
-plt.plot(x_values, y_values, marker='o', label='Sine wave')
-plt.xlabel('Time')
-plt.ylabel('Number')
+fig, ax = plt.subplots()
+ax.plot(x_values, y_values)
+
+plt.xlabel('Packet send time (ms)')
+plt.ylabel('Packet number')
 plt.title('DDOS using python')
 
-# Display the graph
-plt.show()
+# Make the y axis as logarithmic scale
+ax.set_yscale('log')
+
+fig.savefig("Syn_pkts_p.png")
---
+++ vols/monitor/graph_c.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+
+# Read data from the text file
+with open('monitors_ping_c.txt', 'r') as file:
+    lines = file.readlines()
+
+# Extract the x and y values from the lines
+x_values = []  # time
+y_values = []  # number of packets
+timer = 0
+counter = 0
+for line in lines:
+    counter += 1
+    data = line.split("\t")
+    time = float(data[1])
+    x_values .append(time)
+    y_values .append(data[0])
+
+fig, ax = plt.subplots()
+ax.plot(x_values, y_values)
+
+plt.xlabel('Packets RTT (ms)')
+plt.ylabel('Packet number')
+plt.title('DDOS using python')
+
+# Make the y axis as logarithmic scale
+ax.set_yscale('log')
+
+fig.savefig("Monitor_pkts_c.png")
